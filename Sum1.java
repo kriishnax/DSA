d9e91040f09fb57653d6127053d2da770aa78863
Krishna <krishnasusari@gmail.com>
2025-08-26 15:34:57 +0530

    ffdhgdg

---
+++ Sum1.java
@@ -0,0 +1,14 @@
+//sum of n natural numbers
+import java.util.*;
+public class Sum1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number to find the sum of n natural numbers:");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            sum = sum + i;
+        }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+    }
+}
