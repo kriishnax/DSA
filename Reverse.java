a2abb23d19fa0b6865b9115df8cc107ccc4141c4
Krishna <krishnasusari@gmail.com>
2025-08-26 16:20:00 +0530

    add loops

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Reverse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a new number");
+        int n = sc.nextInt();
+        while (n>0) {
+            int lastdigit = n%10;
+            System.out.print(lastdigit);
+            n/=10;
+
+            
+        }
+
+    }
+}
