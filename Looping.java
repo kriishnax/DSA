e74f67db19d99d468f7b74b1a260e9a5878df340
Krishna <krishnasusari@gmail.com>
2025-08-26 15:25:07 +0530

    Looping concept in Java

---
+++ Looping.java
@@ -0,0 +1,15 @@
+//Program to print n numbers using while loop
+
+import java.util.*;
+public class Looping {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range:");
+        int range = sc.nextInt();
+        int counter = 1;
+        while(counter<=range){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
